888db9fd27eed3355fadfae11dfe4826c9dc5b22
Irfan <irfancesir93@gmail.com>
2022-12-22 18:46:19 +0100

    navbar, header, about section

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 's9spa969',
+    dataset: 'production'
+  }
+})
---
+++ frontend_react/src/container/about/About.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
+import "./About.scss";
+
+const abouts = [
+  {
+    title: "Web Design",
+    description: "I'm mediocre web designer, but google search does wonders.",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Frontend Development",
+    description: "I'm an eager beginner hungry for knowledge.",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Backend Enthusiast",
+    description: "After I master Frontend I would love to move towards backend",
+    imgUrl: images.about03,
+  },
+];
+
+function About() {
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Apps </span>
+        <br />
+        Mean <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default About;
