c390f39665cd01929905aa7e920beb1034a17d8b
Irfan <irfancesir93@gmail.com>
2022-12-22 19:52:13 +0100

    fixed header section

--- frontend_react/src/container/header/Header.jsx
+++ frontend_react/src/container/header/Header.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import "./Header.scss";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+
+import { images } from "../../constants/";
+import "./Header.scss";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 
 function Header() {
-  const scaleVariants = {
-    whileInView: {
-      scale: [0, 1],
-      opacity: [0, 1],
-      transition: {
-        duration: 1,
-        ease: "easeInOut",
-      },
-    },
-  };
-
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app_header app__flex">
       <motion.div
-        whileInView={{ x: [-100, -0], opacity: [0, 1] }}
-        transition={{ duration: 1 }}
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
@@ -32,9 +33,9 @@ function Header() {
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className="p-text">My dream is to develop. </p>
+            <p className="p-text">My dream is to develop</p>
             <p className="p-text">
-              Open for full-time, part-time and freelance work.
+              Open for full-time, part-time and freelance
             </p>
           </div>
         </div>
@@ -50,7 +51,7 @@ function Header() {
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
           src={images.circle}
-          alt="profile-circle"
+          alt="profile_circle"
           className="overlay_circle"
         />
       </motion.div>
@@ -62,7 +63,7 @@ function Header() {
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="circle" />
+            <img src={circle} alt="profile_bg" />
           </div>
         ))}
       </motion.div>
