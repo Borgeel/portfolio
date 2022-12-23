6a75697b467d7f3d19cd5c54fb1019eb99505ad3
Irfan <irfancesir93@gmail.com>
2022-12-23 22:31:06 +0100

    skills section

--- frontend_react/src/container/skills/Skills.jsx
+++ frontend_react/src/container/skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
+import ReactTooltip from "react-tooltip";
 
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
@@ -8,7 +9,34 @@ import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 function Skills() {
-  return <div></div>;
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = "[_type == 'skills']";
+
+    client.fetch(query).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text"> Skills & Experiences </h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            ></motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
 }
 
 export default Skills;
